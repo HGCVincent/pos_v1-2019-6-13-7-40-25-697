a269bf8f3f13765116afe77e5a949750ee5b48eb
Vincent <1061271330@qq.com>
2019-07-10 23:19:46 +0800

    add function getCountOfItems

--- main/main.js
+++ main/main.js
@@ -10,3 +10,28 @@ const isExited = (barcode) => {
     })
     return isExited;
 }
+
+const getCountOfItems = (barcodes) =>{
+    const cart = {};
+    barcodes.forEach(barcode =>{
+        let index = barcode.indexOf('-');
+        let subBarcode = '';
+        if (index > -1){
+            subBarcode = barcode.substring(0,index);
+            if (isExited(subBarcode)){
+                if (cart[subBarcode]) {
+                    cart[subBarcode] += parseFloat(barcode.substring(index+1));
+                }
+                else cart[subBarcode] = parseFloat(barcode.substring(index+1));
+            }
+        }else if (index === -1){
+            if (isExited(barcode)){
+                if (cart[barcode]) {
+                    cart[barcode]++;
+                }
+                else cart[barcode] = 1;
+            }
+        }
+    })
+    return cart;
+}
